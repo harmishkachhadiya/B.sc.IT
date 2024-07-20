69edf787ea4fa09350881b798600aa5f08cda8ab
harmishkachhadiya <23bmiit120@gmail.com>
2024-07-20 14:53:48 +0530

    hk

--- Sem-3/IT4024 - Data Structures/Practical/Practical-1/Q5_String_char_count.cpp
+++ Sem-3/IT4024 - Data Structures/Practical/Practical-1/Q5_String_char_count.cpp
@@ -1,1 +1,25 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    string inputString;
+    char characterToCount;
+    int count = 0;
+
+    cout << "Enter the string: ";
+    cin >> inputString;
+
+    cout << "Enter the character to count: ";
+    cin >> characterToCount;
+
+    for (int i = 0; i < inputString.length(); i++) {
+        if (inputString[i] == characterToCount) {
+            count++;
+        }
+    }
+  
+    cout << "The character '" << characterToCount << "' occurs " << count << " times in the string." << endl;
+
+    return 0;
+}
